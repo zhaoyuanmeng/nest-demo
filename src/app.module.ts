280f3f456abd74b64d4ca8430bc2f2eaef47a29e
zhaoyuanmeng <1426593075@qq.com>
2022-07-04 17:43:23 +0800

    feat: 添加数据库的配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [PostsModule], //导入模块的列表，如果需要使用其他模块的服务，需要通过这里导入
+  imports: [TypeOrmModule.forRoot()], //导入模块的列表，如果需要使用其他模块的服务，需要通过这里导入
   controllers: [AppController], //处理http请求，包括路由控制，向客户端返回响应，将具体业务逻辑委托给providers处理；
   providers: [AppService], //Nest.js注入器实例化的提供者（服务提供者），处理具体的业务逻辑，各个模块之间可以共享
 })
